cf3be6e5123b90f59bd21fe2f0a4d806d84556e7
DHATCHANA MOORTHI <111693185+Dhatchanamoorthi8@users.noreply.github.com>
2023-08-09 18:09:19 +0530

    Add files via upload

---
+++ Bootstrap/ex.js
@@ -0,0 +1,12 @@
+// document.write(hello);
+
+
+// const person ={first:"name", last:"name"};
+// console.log(person)
+
+
+var person ={
+    age:20,
+    name:"moorthi",
+    }
+    console.log(person)
