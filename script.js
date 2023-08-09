871ed96e361c4d8080f3221ccf9276ce5c2c0b3f
DHATCHANA MOORTHI <111693185+Dhatchanamoorthi8@users.noreply.github.com>
2023-08-09 18:10:23 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById("hello").onclick = function(){
+    if(confirm("Thank You!!")){
+        document.location="index.html"
+    }else{
+        confirm("Please accept The cookies")
+    }
+} 
+document.getElementById("hello1").onclick = function(){
+    if(confirm("Please accept The cookies")){
+    }else{
+        confirm("Please accept The cookies")
+    }
+
+} 
+
+document.getElementById("demo").innerText = "thank you";
